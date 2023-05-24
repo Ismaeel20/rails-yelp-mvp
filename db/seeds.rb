a2fe4d87913cb27fe14978b762363b1392ebff2c
ismaeel haque ismaeelhaque10@gmail.com <ismaeelhaque10@gmail.com>
2023-05-24 16:51:56 +0100

    Incomplete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+nobu = {name: 'Nobu', address: 'London', phone_number: '726768766', category: 'french' }
+wing_stop = {name: 'Wingstop', address: 'London', phone_number: '366687682', category: 'japanese' }
+sahara_grill = {name: 'Sahara Grill', address: 'London', phone_number: '74974598', category: 'belgian' }
+kfc = {name: 'KFC', address: 'London', phone_number: '67269992374', category: 'french' }
+mcdonalds = {name: 'Mcdonalds', address: 'London', phone_number: '87868554', category: 'italian' }
+
+[nobu, wing_stop, sahara_grill, kfc, mcdonalds].each do |res|
+  restaurant = Restaurant.create!(res)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished Mate'
